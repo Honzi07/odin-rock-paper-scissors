ae375bd104f6e605d582931ed53987c9e8bb47b9
Honzi07 <nyisztor.zsolt@gmail.com>
2022-05-18 21:11:56 +0200

    You can play one round against the computer

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function computerPlay() {
-    let choice = ['Rock', 'Paper', 'Scissors'];
+    const choice = ['Rock', 'Paper', 'Scissors'];
     let num = Math.floor(Math.random() * 3);
     return(choice[num]) 
 }
 
+// Better use switch statement?
+function playRound(player, computer) {
+    if (player === computer) {
+        result = 'Draw!'
+    }
+    if (player === 'Rock' && computer === 'Paper') {
+        result = `You Lost! ${computer} beats ${player}`
+    }
+    if (player === 'Rock' && computer === 'Scissors') {
+        result = `YAY! You Win! ${player} beats ${computer}`
+    }
+    if (player === 'Paper' && computer === 'Scissors') {
+        result = `You Lost! ${computer} beats ${player}`
+    }
+    if (player === 'Paper' && computer === 'Rock') {
+        result = `YAY! You Win! ${player} beats ${computer}`
+    }
+    if (player === 'Scissors' && computer === 'Rock') {
+        result = `You Lost! ${computer} beats ${player}`
+    }
+    if (player === 'Scissors' && computer === 'Paper') {
+        result = `YAY! You Win! ${player} beats ${computer}`
+    }
+}
+
+const player = 'Rock'
+const computer = computerPlay();
