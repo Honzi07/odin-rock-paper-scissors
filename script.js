5fe5b9cfe13f52f96b097b431e417c3efe9f5453
Honzi07 <nyisztor.zsolt@gmail.com>
2022-05-22 21:49:09 +0200

    Added scoring and some user input validation the game is functional

--- script.js
+++ script.js
@@ -1,33 +1,70 @@
 function computerPlay() {
-    const choice = ['Rock', 'Paper', 'Scissors'];
-    let num = Math.floor(Math.random() * 3);
-    return(choice[num]) 
+  const choice = ["Rock", "Paper", "Scissors"];
+  let num = Math.floor(Math.random() * 3);
+  return choice[num];
 }
+
+let playerScore = 0;
+let computerScore = 0;
 
 // Better use switch statement?
 function playRound(player, computer) {
-    if (player === computer) {
-        result = 'Draw!'
-    }
-    if (player === 'Rock' && computer === 'Paper') {
-        result = `You Lost! ${computer} beats ${player}`
-    }
-    if (player === 'Rock' && computer === 'Scissors') {
-        result = `YAY! You Win! ${player} beats ${computer}`
-    }
-    if (player === 'Paper' && computer === 'Scissors') {
-        result = `You Lost! ${computer} beats ${player}`
-    }
-    if (player === 'Paper' && computer === 'Rock') {
-        result = `YAY! You Win! ${player} beats ${computer}`
-    }
-    if (player === 'Scissors' && computer === 'Rock') {
-        result = `You Lost! ${computer} beats ${player}`
-    }
-    if (player === 'Scissors' && computer === 'Paper') {
-        result = `YAY! You Win! ${player} beats ${computer}`
-    }
+  computer = computerPlay();
+  player = playerChoice();
+  if (player === computer) {
+    result = "Draw!";
+  }
+  if (player === "Rock" && computer === "Paper") {
+    result = `You Lost! ${computer} beats ${player}`;
+    computerScore++;
+  }
+  if (player === "Rock" && computer === "Scissors") {
+    result = `YAY! You Win! ${player} beats ${computer}`;
+    playerScore++;
+  }
+  if (player === "Paper" && computer === "Scissors") {
+    result = `You Lost! ${computer} beats ${player}`;
+    computerScore++;
+  }
+  if (player === "Paper" && computer === "Rock") {
+    result = `YAY! You Win! ${player} beats ${computer}`;
+    playerScore++;
+  }
+  if (player === "Scissors" && computer === "Rock") {
+    result = `You Lost! ${computer} beats ${player}`;
+    computerScore++;
+  }
+  if (player === "Scissors" && computer === "Paper") {
+    result = `YAY! You Win! ${player} beats ${computer}`;
+    playerScore++;
+  }
+  console.log(result);
 }
 
-const player = 'Rock'
-const computer = computerPlay();
+function winner() {
+  if (playerScore > computerScore) {
+    alert("You win the game!");
+  } else if (playerScore < computerScore) {
+    alert("The computer win the game!");
+  } else alert("The game is draw");
+}
+
+function game() {
+  for (let i = 1; i <= 5; i++) {
+    playRound();
+  }
+  console.log(`Your score ${playerScore}`);
+  console.log(`Computer Score ${computerScore}`);
+  winner();
+}
+
+function playerChoice() {
+  let player = prompt("Select your weapon! Rock, Paper, Scissors");
+  while (player == '') {
+      player = prompt("Select your weapon! Rock, Paper, Scissors");
+  }
+  player = player[0].toUpperCase() + player.slice(1).toLowerCase();
+  return player;
+}
+
+game();
